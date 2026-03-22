366ce0ed5184627ea2c9b6f2892773977d09512b
Farid Gaoua <47525617+faridgaoua@users.noreply.github.com>
2026-03-22 15:23:31 +0100

    change color

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app",
+  site: "https://gleeful-vacherin-109992.netlify.app",
   integrations: [icon(), sitemap()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -35,18 +35,18 @@ export default {
     extend: {
       colors: {
         transparent: "transparent",
-        primary: {
-          50: "#fdf2f8",
-          100: "#fce7f3",
-          200: "#fbcfe8",
-          300: "#f9a8d4",
-          400: "#f472b6",
-          500: "#E2187D",
-          600: "#db2777",
-          700: "#be185d",
-          800: "#9d174d",
-          900: "#831843",
-          950: "#500724",
+      primary: {
+          50: "#fff7ed",
+          100: "#ffedd5",
+          200: "#fed7aa",
+          300: "#fdba74",
+          400: "#fb923c",
+          500: "#f97316", // Main Brand Orange
+          600: "#ea580c",
+          700: "#c2410c",
+          800: "#9a3412",
+          900: "#7c2d12",
+          950: "#431407",
         },
         neutral: {
           50: "#f8fafc",
